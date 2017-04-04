29970d12ec647e8bf5975a736802f8e09e6a3727
Stefany288 <stefamoron25@gmail.com>
2017-04-04 02:37:12 -0500

    stefany
    Adelanto del proyecto

---
+++ src/main/java/edu/eci/arsw/msgbroker/model/Participante.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.msgbroker.model;
+
+/**
+ *
+ * @author User
+ */
+public class Participante {
+    
+    String nombre;
+    public Participante(String nombre){
+        this.nombre= nombre;
+    
+    }
+    
+    public String getNombre(){
+    return nombre;
+    }
+    
+    
+}
---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,15 @@
+nombresParticipantes=[];
+
+
+
+
+function nombre(){
+    $('#nombreNick').prepend('<inpunt>'+"hola!!!!!"+'</inpunt>');
+    alert("siiii");
+}
+$(document).ready(
+        
+        function () {
+            console.info('loading script!...');
+        }
+);
